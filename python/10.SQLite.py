a82d56006af78014470722841961394574e99933
César <cesarlpb@gmail.com>
2024-08-05 18:05:07 +0200

    estructura de conector de db

--- python/10.SQLite.py
+++ python/10.SQLite.py
@@ -1,4 +1,29 @@
 import sqlite3
 
 class DBConnector():
-    pass
+    
+    # constructor
+    # ruta a la db de sqlite
+    def __init__(self) -> None:
+        pass
+    
+    # create table
+    def crear_tabla(params, sql):
+        pass
+
+    # podemos pasarle una fuente de datos como un CSV:
+    def seed(datos):
+        pass
+    
+    # leer datos
+      # order by... 
+    def select(id):
+        pass
+    
+    # actualizar
+    def update(id):
+        pass
+    
+    # borrar por id
+    def delete(id):
+        pass
